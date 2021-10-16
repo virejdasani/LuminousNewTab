64e0ed58c1b5842f9944cc782ec5f2488ff31884
Virej Dasani <virej@Virejs-MacBook-Air.local>
2021-10-16 22:19:24 +0530

    Add new modal

--- LuminousNewTab/src/index.js
+++ LuminousNewTab/src/index.js
@@ -203,13 +203,6 @@ openSearchButton.addEventListener("click", function (event) {
   window.location.replace(currentEngine.url);
 });
 
-// opening and closing modal
-document.querySelectorAll(".toggleModal").forEach((x) => {
-  x.addEventListener("click", () =>
-    document.querySelector("#modal").classList.toggle("modalShown")
-  );
-});
-
 // closing modal on esc button
 document.addEventListener("keydown", function (event) {
   if (
@@ -280,3 +273,29 @@ window.onload = function () {
       }
     });
 };
+
+// select the open-btn button
+let openBtn = document.getElementById("open-btn");
+// select the modal-background
+let modalBackground = document.getElementById("modal-background");
+// select the close-btn
+let closeBtn = document.getElementById("close-btn");
+
+// shows the modal when the user clicks open-btn
+openBtn.addEventListener("click", function () {
+  modalBackground.style.display = "block";
+});
+
+// hides the modal when the user clicks close-btn
+closeBtn.addEventListener("click", function () {
+  modalBackground.style.display = "none";
+});
+
+// hides the modal when the user clicks outside the modal
+window.addEventListener("click", function (event) {
+  // check if the event happened on the modal-background
+  if (event.target === modalBackground) {
+    // hides the modal
+    modalBackground.style.display = "none";
+  }
+});
