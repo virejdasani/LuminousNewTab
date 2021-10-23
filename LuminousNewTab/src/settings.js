82b0c7a2fb5bffd5c9ba803cedfa88057721ba6d
Khalid shammout <5alidshammout@gmail.com>
2021-10-23 07:02:46 +0300

    bugfix

--- LuminousNewTab/src/settings.js
+++ LuminousNewTab/src/settings.js
@@ -1,175 +1,172 @@
 // For custom search engines
 const engines = [
-  {
-    name: "Google",
-    url: "https://google.com/",
-    search: "https://google.com/search?q=",
-    icon: "../assets/img/googleLogo.png",
-    id: "google",
-  },
-  {
-    name: "DuckDuckGo",
-    url: "https://duckduckgo.com/",
-    search: "https://duckduckgo.com/?q=",
-    icon: "../assets/img/ddgLogo.png",
-    id: "ddg",
-  },
+	{
+		name: 'Google',
+		url: 'https://google.com/',
+		search: 'https://google.com/search?q=',
+		icon: '../assets/img/googleLogo.png',
+		id: 'google',
+	},
+	{
+		name: 'DuckDuckGo',
+		url: 'https://duckduckgo.com/',
+		search: 'https://duckduckgo.com/?q=',
+		icon: '../assets/img/ddgLogo.png',
+		id: 'ddg',
+	},
 ];
 
 // Set search engine
 function updateSearch() {
-  var currentEngine = engines.find((element) => element.name == engine);
-  document.getElementById(
-    "searchBox"
-  ).style.backgroundImage = `url("${currentEngine.icon}")`;
-  document.getElementById("searchBox").textContent =
-    "Search " + currentEngine.name;
-  document.getElementById("searchButton").textContent =
-    currentEngine.name + " Search";
-  document.getElementById("openSearch").textContent =
-    "Open " + currentEngine.name;
-  document.getElementById("currentEngineName").textContent =
-    "Current Search Engine: " + currentEngine.name;
+	var currentEngine = engines.find(element => element.name == engine);
+	document.getElementById(
+		'searchBox'
+	).style.backgroundImage = `url("${currentEngine.icon}")`;
+	document.getElementById('searchBox').textContent =
+		'Search ' + currentEngine.name;
+	document.getElementById('searchButton').textContent =
+		currentEngine.name + ' Search';
+	document.getElementById('openSearch').textContent =
+		'Open ' + currentEngine.name;
+	document.getElementById('currentEngineName').textContent =
+		'Current Search Engine: ' + currentEngine.name;
 }
 
 var engine;
 
-if (typeof browser === "undefined") {
-  // we're running on chrome
-  chrome.storage.local.get("searchEngine", function (res) {
-    engine = res.searchEngine || "Google"; // google is the default
-    updateSearch();
-  });
+if (typeof browser === 'undefined') {
+	// we're running on chrome
+	chrome.storage.local.get('searchEngine', function (res) {
+		engine = res.searchEngine || 'Google'; // google is the default
+		updateSearch();
+	});
 } else {
-  // we're running on firefox
-  browser.storage.local.get("searchEngine").then((res) => {
-    engine = res.searchEngine || "Google"; // google is the default
-    updateSearch();
-  });
+	// we're running on firefox
+	browser.storage.local.get('searchEngine').then(res => {
+		engine = res.searchEngine || 'Google'; // google is the default
+		updateSearch();
+	});
 }
 
-document.getElementById("settings_changesearch").onclick = function () {
-  if (!engine) return;
+document.getElementById('settings_changesearch').onclick = function () {
+	if (!engine) return;
 
-  var newEngine = engines.findIndex((element) => element.name == engine) + 1;
-  if (newEngine > engines.length - 1) newEngine = 0; // wrap around to the first engine
-  engine = engines[newEngine].name;
+	var newEngine = engines.findIndex(element => element.name == engine) + 1;
+	if (newEngine > engines.length - 1) newEngine = 0; // wrap around to the first engine
+	engine = engines[newEngine].name;
 
-  if (typeof browser === "undefined") {
-    // we're running on chrome
-    chrome.storage.local.set({ searchEngine: engine }, function () {
-      updateSearch();
-    });
-  } else {
-    // we're running on firefox
-    browser.storage.local.set({ searchEngine: engine }).then(updateSearch());
-  }
+	if (typeof browser === 'undefined') {
+		// we're running on chrome
+		chrome.storage.local.set({ searchEngine: engine }, function () {
+			updateSearch();
+		});
+	} else {
+		// we're running on firefox
+		browser.storage.local.set({ searchEngine: engine }).then(updateSearch());
+	}
 };
 
 // When search button is clicked
-searchButton.addEventListener("click", function (event) {
-  // Don't reload the page
-  // Without this, window.location.replace is not working
-  event.preventDefault()
+searchButton.addEventListener('click', function (e) {
+	// Don't reload the page
+	// Without this, window.location.replace is not working
+	e.preventDefault();
 
-  // Google search
-  searchBox.value !== "" ? window.location.replace("https://www.google.com/search?q=" + searchBox.value) : (function () {
-      searchBox.placeholder = "Enter Text Before Searching!!"
-      searchBox.classList.add("angry")
-  })()
-  setTimeout(() => {
-      searchBox.classList.remove("angry")
-      searchBox.placeholder = "Search"
-  }, 1000)
-})
-  event.preventDefault();
+	var currentEngine = engines.find(element => element.name == engine);
+	// Google search
+	searchBox.value != '' && searchBox.value != null
+		? window.location.replace(currentEngine.search + searchBox.value)
+		: (function () {
+				searchBox.placeholder = 'Enter Text Before Searching!!';
+				searchBox.classList.add('angry');
+		  })();
 
-  // Search
-  var currentEngine = engines.find((element) => element.name == engine);
-
-  window.location.replace(currentEngine.search + searchBox.value);
+	setTimeout(() => {
+		searchBox.classList.remove('angry');
+		searchBox.placeholder = 'Search';
+	}, 1000);
 });
 
-openSearchButton.addEventListener("click", function (event) {
-  // Don't reload the page
-  event.preventDefault();
+openSearchButton.addEventListener('click', function (event) {
+	// Don't reload the page
+	event.preventDefault();
 
-  var currentEngine = engines.find((element) => element.name == engine);
-  window.location.replace(currentEngine.url);
+	var currentEngine = engines.find(element => element.name == engine);
+	window.location.replace(currentEngine.url);
 });
 
 var displayBookmarks;
 
 // By default, bookmarks are shown
-if (typeof browser === "undefined") {
-  // we're running on chrome
-  chrome.storage.local.get("showBookmarks", function (res) {
-    if (res.showBookmarks == false) {
-      displayBookmarks = false;
-    } else {
-      displayBookmarks = true;
-    }
-    toggleBookmarksButton.checked = displayBookmarks;
-    updateBookmarkPrefs();
-  });
+if (typeof browser === 'undefined') {
+	// we're running on chrome
+	chrome.storage.local.get('showBookmarks', function (res) {
+		if (res.showBookmarks == false) {
+			displayBookmarks = false;
+		} else {
+			displayBookmarks = true;
+		}
+		toggleBookmarksButton.checked = displayBookmarks;
+		updateBookmarkPrefs();
+	});
 } else {
-  // we're running on firefox
-  browser.storage.local.get("showBookmarks").then((res) => {
-    displayBookmarks = res.showBookmarks || true; // bookmarks are shown by default
-    toggleBookmarksButton.checked = res.showBookmarks || true;
-    updateBookmarkPrefs();
-  });
+	// we're running on firefox
+	browser.storage.local.get('showBookmarks').then(res => {
+		displayBookmarks = res.showBookmarks || true; // bookmarks are shown by default
+		toggleBookmarksButton.checked = res.showBookmarks || true;
+		updateBookmarkPrefs();
+	});
 }
 
-document.getElementById("toggleBookmarks").addEventListener("click", () => {
-  // console.log("clicked");
+document.getElementById('toggleBookmarks').addEventListener('click', () => {
+	// console.log("clicked");
 
-  if (displayBookmarks == true) {
-    // console.log("hiding bookmarks");
+	if (displayBookmarks == true) {
+		// console.log("hiding bookmarks");
 
-    if (typeof browser === "undefined") {
-      // we're running on chrome
-      chrome.storage.local.set({ showBookmarks: false }, function () {
-        toggleBookmarksButton.checked = false;
-        displayBookmarks = false;
-        updateBookmarkPrefs();
-      });
-    } else {
-      // we're running on firefox
-      browser.storage.local.set({ showBookmarks: false });
-      toggleBookmarksButton.checked = false;
-      displayBookmarks = false;
-      updateBookmarkPrefs();
-    }
-  } else {
-    // console.log("showing bookmarks");
+		if (typeof browser === 'undefined') {
+			// we're running on chrome
+			chrome.storage.local.set({ showBookmarks: false }, function () {
+				toggleBookmarksButton.checked = false;
+				displayBookmarks = false;
+				updateBookmarkPrefs();
+			});
+		} else {
+			// we're running on firefox
+			browser.storage.local.set({ showBookmarks: false });
+			toggleBookmarksButton.checked = false;
+			displayBookmarks = false;
+			updateBookmarkPrefs();
+		}
+	} else {
+		// console.log("showing bookmarks");
 
-    if (typeof browser === "undefined") {
-      // we're running on chrome
-      chrome.storage.local.set({ showBookmarks: true }, function () {
-        toggleBookmarksButton.checked = true;
-        displayBookmarks = true;
-        updateBookmarkPrefs();
-      });
-    } else {
-      // we're running on firefox
-      browser.storage.local.set({ showBookmarks: true });
-      toggleBookmarksButton.checked = true;
-      displayBookmarks = true;
-      updateBookmarkPrefs();
-    }
-  }
+		if (typeof browser === 'undefined') {
+			// we're running on chrome
+			chrome.storage.local.set({ showBookmarks: true }, function () {
+				toggleBookmarksButton.checked = true;
+				displayBookmarks = true;
+				updateBookmarkPrefs();
+			});
+		} else {
+			// we're running on firefox
+			browser.storage.local.set({ showBookmarks: true });
+			toggleBookmarksButton.checked = true;
+			displayBookmarks = true;
+			updateBookmarkPrefs();
+		}
+	}
 });
 
 // This is to show or hide the bookmarks based on user prefs
 function updateBookmarkPrefs() {
-  if (displayBookmarks) {
-    document.getElementById("bookmarks").style.display = "block";
-    document.getElementById("greeting").style.marginTop = "155px";
-  } else {
-    document.getElementById("bookmarks").style.display = "none";
-    document.getElementById("greeting").style.marginTop = "15rem";
-  }
+	if (displayBookmarks) {
+		document.getElementById('bookmarks').style.display = 'block';
+		document.getElementById('greeting').style.marginTop = '155px';
+	} else {
+		document.getElementById('bookmarks').style.display = 'none';
+		document.getElementById('greeting').style.marginTop = '15rem';
+	}
 }
 
 // // For bookmark number
